ab8f3b340dae12ecf46966ac3c6051b6ee6aa70c
nasseralkmim <nasser.alkmim@gmail.com>
2016-11-12 20:43:13 -0200

    test

--- diffuspy/stiffness.py
+++ diffuspy/stiffness.py
@@ -9,6 +9,7 @@ def K_matrix(model, material):
     for e, conn in enumerate(model.CONN):
         xyz = model.XYZ[conn]
         surf = model.surf_of_ele[e]
+        # adding a comment
         try:
             cndtvt = material.cndtvt[surf]
         except:
