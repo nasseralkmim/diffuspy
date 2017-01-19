deb7f4ee59734e3d7927fd103049fd83ab08a5d9
nasseralkmim <nasser.alkmim@gmail.com>
2017-01-19 10:34:11 -0200

    added comments for units and physical names

--- test/ex5_heatSt.py
+++ test/ex5_heatSt.py
@@ -8,16 +8,19 @@ model_name = 'patch'
 model = gmsh.Parse(model_name)
 
 s = list(model.surf.keys())
-material = Material(λ={s[0]: 1},
-                    c={s[0]: 1},
-                    ρ={s[0]: 0.1})
+material = Material(λ={s[0]: 1}, # condutivity W/m K
+                    c={s[0]: 1}, # specific heat J/kg K
+                    ρ={s[0]: 0.1}) # density kg/ m3
 
+# Internal heat (q) current density source (σ) in W/ m3
 def σ_q(x1, x2, t=1):
     return 0
 
+# Heat (q) current density assigned at the boundary (bc) in W/m3
 def q_bc(x1, x2, t=1):
     return {1: -20}
 
+# Temperature (T) assigned at boundary (bc) in K or C
 def T_bc(x1, x2, t=1):
     return {3: 50}
 
